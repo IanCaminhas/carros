cc9a7b2fc7b5dc8dbfb8dad4906761ca89a622e5
Ian Caminhas <iancaminhas25@hotmail.com>
2019-07-09 08:02:12 -0300

    mostrar um texto ou Circular progress indicator no botão login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -2,11 +2,18 @@ import 'package:carros_app/domain/login_service.dart';
 import 'package:carros_app/utils/alerts.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
 
-  final loginController = TextEditingController(text:"");
-  final senhaController = TextEditingController(text:"");
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();// permite criar funções para validar login e a senha
+class _LoginPageState extends State<LoginPage> {
+  final loginController = TextEditingController(text: "");
+  final senhaController = TextEditingController(text: "");
+  final GlobalKey<FormState> _formKey = GlobalKey<
+      FormState>(); // permite criar funções para validar login e a senha
+
+  var _progress = false;
 
   @override
   Widget build(BuildContext context) {
@@ -21,26 +28,21 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  String _validateLogin(String text){
-
-    if(text.isEmpty){
-        return "Informe o login";
+  String _validateLogin(String text) {
+    if (text.isEmpty) {
+      return "Informe o login";
     }
     return null;
-
   }
 
-  String _validateSenha(String text){
-
-    if(text.isEmpty){
+  String _validateSenha(String text) {
+    if (text.isEmpty) {
       return "Informe a senha";
     }
-    if(text.length<=2){
+    if (text.length <= 2) {
       return "Senha precisa ter mais de 2 números ";
-
     }
     return null;
-
   }
 
   Widget _body(BuildContext context) {
@@ -62,9 +64,10 @@ class LoginPage extends StatelessWidget {
           TextFormField(
             controller: senhaController,
             validator: _validateSenha,
-            obscureText: true, // para mostrar as bolinhas na senha
-            style: TextStyle(
-                color: Colors.blue, fontSize: 25), // design das letras do input
+            obscureText: true,
+            // para mostrar as bolinhas na senha
+            style: TextStyle(color: Colors.blue, fontSize: 25),
+            // design das letras do input
             decoration: InputDecoration(
                 labelText: 'Senha',
                 labelStyle: TextStyle(color: Colors.black, fontSize: 25.0),
@@ -73,17 +76,22 @@ class LoginPage extends StatelessWidget {
                 hintStyle: TextStyle(color: Colors.black, fontSize: 18)),
           ),
           Container(
-            height: 50.0,// altura do login
-            margin: EdgeInsets.only(top: 20.0),// espaçamento de cima do input de senha e
+            height: 50.0, // altura do login
+            margin: EdgeInsets.only(
+                top: 20.0), // espaçamento de cima do input de senha e
             child: RaisedButton(
-            color: Colors.blue,
-
-            child: Text("Login",
-            style: TextStyle(color: Colors.white, fontSize: 25.0),
-            ),
-                onPressed: () {_onClickLogin(context);}
-            ),
-
+                color: Colors.blue,
+                child: _progress
+                    ? CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation(Colors.white),
+                      )
+                    : Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white, fontSize: 25.0),
+                      ),
+                onPressed: () {
+                  _onClickLogin(context);
+                }),
           ),
         ],
       ),
@@ -91,31 +99,36 @@ class LoginPage extends StatelessWidget {
   }
 
   void _onClickLogin(BuildContext context) async {
-
     final login = loginController.text;
     final senha = senhaController.text;
 
-    print('Login: $login, senha:$senha ');
-
     // se a validação não estiver sendo feita pelo validator, retorne null
-   if(!_formKey.currentState.validate()){
+    if (!_formKey.currentState.validate()) {
       return;
     }
 
-    final testValidationResponse= await LoginService.login(login,senha);
+    print('Login: $login, senha:$senha ');
 
+    setState(() {
+      _progress = true;
+    });
 
-    if(testValidationResponse.isOk()){
+    final testValidationResponse = await LoginService.login(login, senha);
+
+    if (testValidationResponse.isOk()) {
       print("Entrar na home !!!");
     } else {
       // import do pacote utils da classe alerts
-      alert(context,"Error","Ocorreu um erro no login");
+      alert(context, "Error", "Ocorreu um erro no login");
     }
 
- /*   if(login.isEmpty || senha.isEmpty){
+    setState(() {
+      _progress = false;
+    });
+
+    /*   if(login.isEmpty || senha.isEmpty){
 
     }
 */
-
   }
 }
